7c33933d83383b8612ab44aaeb2d73f229362f38
wettingchou <wtchou1209@gmail.com>
2015-10-27 17:33:23 +0800

    use mongodb

--- config/connections.js
+++ config/connections.js
@@ -56,16 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  /*
-  someMongodbServer: {
+  testMongodbServer: {
     adapter: 'sails-mongo',
     host: 'localhost',
     port: 27017,
     // user: 'username',
     // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    database: 'testdb'
   },
-  */
 
   /***************************************************************************
   *                                                                          *
